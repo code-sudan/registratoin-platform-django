636289ba58613d4d507320020e724b9a95eba604
ahmed.elsir <ahmed.elsir@energicityconsulting.com>
2021-12-27 08:03:46 +0000

    starting from scratch

--- registration/models.py
+++ registration/models.py
@@ -1,12 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Register(models.Model):
-    register_id = models.IntegerField()
-    register_email = models.EmailField()
-    register_name = models.CharField(max_length=64)
-    program = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.register_name} choice {self.program}"
+class Student(User):
+    pass
--- registration/views.py
+++ registration/views.py
@@ -1,79 +1,33 @@
 from django.shortcuts import render
 from django.urls import reverse
-from .models import Register
 from django import forms
-
-# Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from secrets import randbelow
-from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+
+
 
 USER_PASSWORD = "codesudan"
 
-
-# Create your views here.
-
-@login_required
 def index(request):
-    return HttpResponse("Hello, world!")
-
-class new_user_form(forms.Form):
-    phone_number = forms.CharField(max_length=12, label="رقم تلفونك")
-
-def user_reg(request):
-    if request.method == "GET":
-        return render(request, "registration/user_reg.html", {
-            "form": new_user_form()
-        })
-    elif request.method == "POST":
-        form = new_user_form(request.POST)
-
-        if form.is_valid():
-            phone_number = form.cleaned_data["phone_number"]
-            try:
-                user=User.objects.create_user(username=phone_number, password=USER_PASSWORD)
-                user.save()
-            except IntegrityError:
-                return render(request, "registration/user_reg.html", {
-                    "form": form,
-                    "message": "المستخدم موجود لدينا"
-                })
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-
-        else:
-            return render(request, "registration/user_reg.html", {
-            "form": new_user_form()
-            })
-
-def user_logout(request):
-    logout(request)
-    return HttpResponse("you're logged out")
+    return HttpResponse("This is index")
 
 
+def login(request):
+    pass
 
+def logout(request):
+    pass
 
-class new_register(forms.Form):
-    register_email = forms.EmailField(label="البريد الإلكتروني")
-    register_name = forms.CharField(label="الإسم كاملا ", max_length=64)
-    program = forms.CharField(label="ما هو البرنامج الذي إخترته؟ ", max_length=64)
+def register_student(request):
+    pass
 
-def program_reg(request):
-    if request.method == "GET":
-        return render(request, "registration/register.html", {
-            "form": new_register()
-        })
-    elif request.method == "POST":
-        form = new_register(request.POST)
+def student_detail(request):
+    pass
 
-        if form.is_valid():
-            register_name = form.cleaned_data["register_name"]
-            register_email = form.cleaned_data["register_email"]
-            program = form.cleaned_data["program"]
-            new_registeration = Register(register_id=0, register_name=register_name, register_email=register_email, program=program)
-            new_registeration.save()
-            return HttpResponse("it's been save to the database successfully")
+def register_in_program(request):
+    pass
