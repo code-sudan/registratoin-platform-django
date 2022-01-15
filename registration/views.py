ca6e1416b9cfe36896cffa4b25075e78e31a2fb0
ahmed.elsir <ahmed.elsir@energicityconsulting.com>
2022-01-15 21:56:37 +0000

    commenting all requests

--- registration/views.py
+++ registration/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Student, Registration
 from .forms import *
-import requests as req
+# import requests as req
 
 # Create your views here.
 
