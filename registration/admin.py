2a2469c1105e55f4ef4f8f153ccb93e9e5061363
ahmed.elsir <ahmed.elsir@energicityconsulting.com>
2022-02-07 12:00:20 +0000

    add dashboard to the admin page

--- registration/admin.py
+++ registration/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import Student, Track, Program, Registration, Batch
+from .models import *
 
 # Register your models here.
 
-admin.site.register(Student)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'program', 'created_at', 'package', 'is_enroll', 'transaction_id')
+    list_filter = ('created_at', 'is_enroll', 'program')
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'email', 'is_complete')
+
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Track)
 admin.site.register(Program)
-admin.site.register(Registration)
+admin.site.register(Registration, RegistrationAdmin)
 admin.site.register(Batch)
+
